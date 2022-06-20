169bfbca69a1961e57dee3ea90805560bb550292
raffay2001 <raffay.ahmed009@gmail.com>
2022-06-20 20:55:28 +0500

    Node Callbacks and Error Handling

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ let rect = require('./rectangle');
 function solveRect(l, b){
     console.log(`Solving for Rectangle with Length: ${l} and Breadth: ${b}`);
 
-    if ( l <= 0 || b <= 0){
-        console.log(`Rectangle Dimensions should be greater than zero !!`)
-    }
-    else{
-        console.log(`Area of the Rectangle is: ${rect.area(l, b)}\nPerimeter of the Rectangle is: ${rect.perimeter(l, b)}`);
-    };
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log(`ERROR: ${err.message}`);
+        }
+        else{
+            console.log(`The area of the rectangle of dimensions l = ${l} and b = ${b} is: ${rectangle.area()}`);
+            console.log(`The perimeter of the rectangle of dimensions l = ${l} and b = ${b}is: ${rectangle.perimeter()}`);
+        }
+    });
+
+    console.log('This statement after the call to rect()');
 
 };
 
 solveRect(2, 4);
-solveRect(3, 5);
-solveRect(0, 5);
-solveRect(-3, 5);
+// solveRect(3, 5);
+// solveRect(0, 5);
+// solveRect(-3, 5);
 
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,25 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x, y) => (x*y);
+module.exports = (x, y, callback) => {
+
+    if(x <= 0 || y <= 0){
+
+        setTimeout(() => {
+            // here the callback which we are specifying takes the first argument as the error class instance with error message and the second argument as the response
+            callback(new Error(`Rectangle Dimensions should be greater than zero l = ${x} and b = ${y}}`), null);   
+            
+        }, 2000);
+    }
+    
+    else{
+        setTimeout(() => {
+            // here the callback which we are specifying takes the first argument as the error class instance with error message and the second argument as the response
+            callback(null, {
+                perimeter: () => (2*(x+y)),
+                area: () => (x*y)
+            });
+
+        }, 2000);
+    }
+
+};
+
+
